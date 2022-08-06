875c8108bc6471c915a84fbaf98a75de917c0cd6
Toooyounger <1977751861@qq.com>
2022-08-06 09:45:35 +0800

    静态头像的实现，

--- src/views/RightHeader.vue
+++ src/views/RightHeader.vue
@@ -9,6 +9,7 @@
     </div>
     <router-link id="shouYe-a" to="/backView/shouYe">首页</router-link>
     <span id="foam" v-for="route in $route.matched" :key="route.path">{{route.meta.name}}</span>
+    <el-avatar id="TouXiang" shape="square" :size="45" fit="fill" :src="url" />
     <div class="seg"></div>
   </div>
 
@@ -20,7 +21,12 @@ import {  Fold } from '@element-plus/icons-vue'
 
 <script>
 export default {
-  name: "RightHeader"
+  name: "RightHeader",
+  data(){
+    return{
+      url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
+    }
+  }
 }
 </script>
 
@@ -49,6 +55,15 @@ export default {
   #foam{
     color: rgb(178,168,190);
   }
-
+  #TouXiang{
+    position: absolute;
+    right: 15px;
+    top: 3px;
+    cursor: pointer;
+    transition: .35s ease;
+  }
+  #TouXiang:hover{
+    transform: scale(1.1);
+  }
 
 </style>
