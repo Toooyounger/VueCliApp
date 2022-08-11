e600800b43ced33c092bc6ccf35c1172fcc66743
taozekang <364142700@qq.com>
2022-08-11 15:07:42 +0800

    1.基本功能已经实现，新增了删除医生和修改医生的功能和界面。 2.修改医生部分仍然存在传递参数的问题，删除功能仅通过了postman测试，还没有再网页测试功能。

--- src/http/modules/doctor.js
+++ src/http/modules/doctor.js
@@ -8,3 +8,11 @@ export function insertMsg(url,params){
 export function getDoctorMsg(url,params){
     return get(url,params);
 }
+
+export function deleteDoctor(url,params){
+    return post(url,params);
+}
+
+export function updateDoctor(url,params){
+    return post(url,params);
+}
